4b2c7e2db871c83e33b703765e4bd00651e504bb
Ethosa <social.ethosa@gmail.com>
2023-08-30 10:41:48 +0700

    final changes

--- main.py
+++ main.py
@@ -25,12 +25,15 @@ def auth():
     """
     User authorization
     """
+    # Get query parameters
     username = request.args.get('username')
     password = request.args.get('password')
+    # valid data
     if username == 'admin' and password == 'admin':
       response = jsonify({'response': 'success'})
     else:
       response = jsonify({'response': 'failure', 'message': 'Неправильный логин или пароль'})
+    # CORS
     response.headers.add("Access-Control-Allow-Origin", "*")
     response.headers.add("Access-Control-Allow-Headers", "*")
     response.headers.add("Access-Control-Allow-Methods", "*")
